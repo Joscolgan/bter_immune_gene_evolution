c7a18b7fb34bf60f213d671ed567b4afcbdf2dbc
joscolgan <tcolgan@tcd.ie>
2022-08-25 22:55:49 +0200

    Updated run_bowtie2.sh code

--- 03_data_alignment/run_bowtie2.sh
+++ 03_data_alignment/run_bowtie2.sh
@@ -5,6 +5,20 @@
 #SBATCH -A mulif005c
 #SBATCH -p ProdQ
 
+#############################################################################################
+## Author: Sarah Larragy, Joe Colgan (joscolgan)                 Program: run_bowtie2.sh
+##
+## Date: 28/06/22
+##
+## Introduction:
+## The purpose of this script is to take fastp-filtered FASTQ sequences and align
+## against a reference genome assembly using the short read aligner Bowtie2.
+## The script generates a compressed alignment file (BAM format) for each samples.
+## The output files are sorted using samtools.
+## 
+#############################################################################################
+
+
 for file in input/*1.fq.gz;
 do
 reverse="$(echo "$file" |cut -d '_' -f 1-5)_2.fq.gz";
@@ -14,9 +28,9 @@ bowtie2 --rg-id "$output" \
 --rg SM:"$output" \
 --rg LB:library1 \
 --rg PL:ILLUMINA \
---rg DS:HiSeq2500 \
+--rg DS:Novaseq6000 \
 --local -p 40 \
--X 1000 -x data/genome/bterr_1.0 \
+-X 1000 -x data/genome/bter_1.0 \
 -1 <(zcat "$file") \
 -2 <(zcat "$reverse") | \
 samtools view -bS | samtools sort -o results/"$output".bam -;
