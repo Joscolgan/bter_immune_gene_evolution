4b18bd0594ded1e4d6c1235f768831a3853aca85
joscolgan <tcolgan@tcd.ie>
2022-08-25 23:29:27 +0200

    Added header

--- 05_population_assessment/run_admixture.sh
+++ 05_population_assessment/run_admixture.sh
@@ -5,8 +5,22 @@
 #SBATCH -A mulif005c
 #SBATCH -p ProdQ
 
+#############################################################################################
+## Author: Sarah Larragy, Joe Colgan (joscolgan)             Program: run_admixture.sh
+##
+## Date: 28/06/22
+##
+## Introduction:
+## The purpose of this script is to take a plink.bed file generated from a filtered VCF
+## containing positions of polymorphic sites in a natural bumblebee population. This script
+## run the software tool ADMIXTURE to determine co-ancestry shared amongst samples in the 
+## input data file. The software performs cross-validation for each tested population
+## and outputs a log file for each level of K tested.
+##
+#############################################################################################
+
 for K in {1..20};
 do
-admixture --cv results/out_irish_hetremoved_raresnpsremoved.ann_plink.bed \
+admixture --cv results/test.plink.bed \
 -j20 $K | tee results/log${K}.out;
 done
