99d4e3d7dac1d94c2b54a4e702b53448c63ea29c
Joe Colgan <tcolgan@tcd.ie>
2022-09-12 18:05:34 +0100

    Update run_depth_snakemake.sh

--- 07_selection_analysis/03_missing_genes_analysis/run_depth_snakemake.sh
+++ 07_selection_analysis/03_missing_genes_analysis/run_depth_snakemake.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-#SBATCH --time=01:00:00
+#SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH -A mulif005c
 #SBATCH -p ProdQ
 
+
+## Load conda:
 module load conda/2
 
+## Activate conda environment:
 source activate conda_env
 
+## Load R:
 module load r
 
+## Run snakemake 
 snakemake -s calculate_depth.py -p -j 10 --rerun-incomplete
