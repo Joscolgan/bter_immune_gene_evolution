92911dad31871ca622d5f0538ee0b9191cb61874
Joe Colgan <tcolgan@tcd.ie>
2022-09-12 19:21:28 +0100

    Update make_bwa_index.sh

--- 07_selection_analysis/04_cnv_analysis/01_realignment/make_bwa_index.sh
+++ 07_selection_analysis/04_cnv_analysis/01_realignment/make_bwa_index.sh
@@ -5,9 +5,20 @@
 #SBATCH -A mulif005c
 #SBATCH -p ProdQ
 
+################################################################################################
+##
+## Author: Sarah Larragy, Joe Colgan (joscolgan).                    Program: make_bwa_index.sh
+##
+## Date: 20-04-2022
+## 
+## Purpose:
+## This script takes a reference genome assembly in FASTA format from the command line and
+## with BWA creates an index for genome alignment.
+##
+################################################################################################
+
 ## Read in input FASTA file from the command line:
 file=$1
 
 ## Create an index using bwa:
 bwa index "$file"
-
