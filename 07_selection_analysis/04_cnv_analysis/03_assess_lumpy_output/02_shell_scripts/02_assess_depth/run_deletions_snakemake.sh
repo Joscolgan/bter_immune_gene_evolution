1a356699c25556c13a6f13c9faaacd7cff842764
Joe Colgan <tcolgan@tcd.ie>
2022-09-12 20:01:22 +0100

    Update run_deletions_snakemake.sh

--- 07_selection_analysis/04_cnv_analysis/03_assess_lumpy_output/02_shell_scripts/02_assess_depth/run_deletions_snakemake.sh
+++ 07_selection_analysis/04_cnv_analysis/03_assess_lumpy_output/02_shell_scripts/02_assess_depth/run_deletions_snakemake.sh
@@ -5,10 +5,26 @@
 #SBATCH -A mulif005c
 #SBATCH -p ProdQ
 
+#############################################################################################
+##
+## Author: Sarah Larragy, Joe Colgan (joscolgan)       	Program: run_deletions_snakemake.sh
+##
+## Date: 28-06-22
+##
+## Introduction:
+## The purpose of this script is to call a snakefile to estimate read depth for putative
+## deleted sites within the genomes of wild-sampled bumblebee males.
+##
+#############################################################################################
+
+## Load conda:
 module load conda/2
 
+## Activate conda environment:
 source activate conda_env
 
+## Load R:
 module load r
 
+## Run snakefile:
 snakemake -s calculate_deletion_depth.py -p -j 15 --rerun-incomplete
